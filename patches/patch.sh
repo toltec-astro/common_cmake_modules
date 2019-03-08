4e35d1d3c35ba12b3de80cf780cddb74860309f2
Jerry Ma <jerry.ma.nk@gmail.com>
2019-03-07 20:14:42 -0500

    add options

--- patches/patch.sh
+++ patches/patch.sh
@@ -4,5 +4,5 @@ PATCH_GLOB=$1
 for PATCH in `ls ${ROOT}/${PATCH_GLOB} 2>/dev/null`
 do
     echo "patch cmd: $(which patch) --verbose -u -N -p 1 < ${PATCH}"
-    patch --verbose -u -N -p 1 < ${PATCH}
+    $(patch --verbose -u -N -p 1 < ${PATCH}) || echo "patch failed, as it maybe already patched."
 done
