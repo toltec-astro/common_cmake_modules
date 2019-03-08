3746ac55c06da04e84f27bd464d827362fa62351
Jerry Ma <jerry.ma.nk@gmail.com>
2019-03-08 10:14:35 -0500

    split Eigen3 and Ceres

---
+++ Ceres.cmake
@@ -0,0 +1,37 @@
+include_guard(GLOBAL)
+option(USE_INSTALLED_CERES "Use installed Ceres-solver" OFF)
+option(FCH_CERES_TAG "Branch/tag used to fetch Ceres-solver source code" OFF)
+if (USE_INSTALLED_CERES)
+    if (EXISTS ${USE_INSTALLED_CERES})
+        set(Ceres_DIR ${USE_INSTALLED_CERES})
+        message("Use Ceres-solver from ${USE_INSTALLED_CERES}")
+    endif()
+    find_package(Ceres REQUIRED CONFIG)
+    message("Found Ceres-solver in ${Ceres_INCLUDE_DIR}")
+else()
+    if (NOT FCH_CERES_TAG)
+        message("Use Ceres-solver from branch master")
+        set(FCH_CERES_TAG "master")
+    endif()
+    include(FetchContentHelper)
+    FetchContentHelper(ceres GIT "https://github.com/ceres-solver/ceres-solver.git" ${FCH_CERES_TAG}
+        ADD_SUBDIR CONFIG_SUBDIR
+            EIGEN_PREFER_EXPORTED_EIGEN_CMAKE_CONFIGURATION=ON
+            EIGENSPARSE=ON
+            SUITESPARSE=ON
+            CXSPARSE=OFF
+            GFLAGS=OFF
+            MINIGLOG=ON
+            CXX11=ON
+            OPENMP=ON
+            TBB=OFF
+            CXX11_THREADS=OFF
+            EXPORT_BUILD_DIR=ON
+            BUILD_DOCUMENTATION=OFF
+            BUILD_TESTING=OFF
+            BUILD_EXAMPLES=OFF
+            BUILD_BENCHMARKS=OFF
+        PATCH_SUBDIR
+            ${FCH_PATCH_DIR}/patch.sh "ceres*.patch"
+        )
+endif()
---
+++ Eigen3.cmake
@@ -0,0 +1,27 @@
+include_guard(GLOBAL)
+option(USE_INSTALLED_EIGEN3 "Use installed Eigen3" OFF)
+option(FCH_EIGEN3_TAG "Branch/tag used to fetch Eigen3 source code" OFF)
+if (USE_INSTALLED_EIGEN3)
+    if (EXISTS ${USE_INSTALLED_EIGEN3})
+        set(Eigen3_DIR ${USE_INSTALLED_EIGEN3})
+        message("Use Eigen3 from ${USE_INSTALLED_EIGEN3}")
+    endif()
+    find_package(Eigen3 REQUIRED CONFIG)
+    set_target_properties(Eigen3::Eigen PROPERTIES INTERFACE_COMPILE_DEFINITIONS "EIGEN_NO_MALLOC")
+    message("Found Eigen3 in ${EIGEN3_INCLUDE_DIR}")
+else()
+    if (NOT FCH_EIGEN3_TAG)
+        message("Use Eigen3 from branch default")
+        set(FCH_EIGEN3_TAG "default")
+    endif()
+    include(FetchContentHelper)
+    FetchContentHelper(eigen HG "https://bitbucket.org/eigen/eigen" ${FCH_EIGEN3_TAG}
+        ADD_SUBDIR CONFIG_SUBDIR
+            BUILD_TESTING=OFF
+        PATCH_SUBDIR
+            ${FCH_PATCH_DIR}/patch.sh "eigen3*.patch"
+        REGISTER_PACKAGE
+            Eigen3 INCLUDE_CONTENT
+                "set(EIGEN3_FOUND TRUE)\nset(EIGEN3_INCLUDE_DIR \${eigen_SOURCE_DIR})"
+        )
+endif()
