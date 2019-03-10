190787bb8a501cc40f9b111a5b0e691b12342a91
Jerry Ma <jerry.ma.nk@gmail.com>
2019-03-09 20:07:41 -0500

    add gitversion

---
+++ gitversion.cmake
@@ -0,0 +1,21 @@
+function(GenVersionHeader local_dir output_dir)
+    message(STATUS "Resolving GIT Version")
+    set(_build_version "unknown")
+    find_package(Git)
+    if(GIT_FOUND)
+      execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+        WORKING_DIRECTORY "${local_dir}"
+        OUTPUT_VARIABLE _build_version
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+      message( STATUS "GIT hash: ${_build_version}")
+    else()
+      message(STATUS "GIT not found")
+    endif()
+
+    string(TIMESTAMP _time_stamp)
+
+    configure_file(${local_dir}/cmake/gitversion.h.in ${output_dir}/gitversion.h @ONLY)
+endfunction()
