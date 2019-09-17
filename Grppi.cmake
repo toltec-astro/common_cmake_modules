737fc808e707202e6225805d61e7d5b16020c367
Jerry Ma <jerry.ma.nk@gmail.com>
2019-09-17 16:25:18 -0400

    update for kids lali

--- Grppi.cmake
+++ Grppi.cmake
@@ -1,6 +1,6 @@
 include_guard(GLOBAL)
 include(FetchContentHelper)
-FetchContentHelper(grppi GIT "https://github.com/Jerry-Ma/grppi.git" master
+FetchContentHelper(grppi GIT "https://github.com/Jerry-Ma/grppi.git" cpp20
     ADD_SUBDIR CONFIG_SUBDIR
         GRPPI_UNIT_TEST_ENABLE=OFF
         GRPPI_TBB_ENABLE=OFF
@@ -8,7 +8,7 @@ FetchContentHelper(grppi GIT "https://github.com/Jerry-Ma/grppi.git" master
 # grppi with OpenMP backend
 find_package(OpenMP REQUIRED)
 add_library(grppi INTERFACE)
-target_include_directories(grppi INTERFACE ${grppi_SOURCE_DIR}/include)
+target_include_directories(grppi SYSTEM INTERFACE ${grppi_SOURCE_DIR}/include)
 target_link_libraries(grppi INTERFACE OpenMP::OpenMP_CXX)
 target_compile_definitions(grppi INTERFACE "-DGRPPI_OMP")
 add_library(grppi::grppi ALIAS grppi)
--- SpdlogAndFmt.cmake
+++ SpdlogAndFmt.cmake
@@ -12,17 +12,29 @@ FetchContentHelper(spdlog GIT "https://github.com/gabime/spdlog.git" v1.x
 string(TOLOWER "${LOGLEVEL}" loglevel)
 set(SPDLOG_ACTIVE_LEVEL "")
 if (loglevel STREQUAL "trace")
-    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_TRACE")
+    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_LEVEL_TRACE")
 elseif (loglevel STREQUAL "debug")
-    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_DEBUG")
+    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_LEVEL_DEBUG")
 elseif (loglevel STREQUAL "info")
-    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_INFO")
+    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_LEVEL_INFO")
 elseif (loglevel STREQUAL "warn")
-    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_WARN")
+    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_LEVEL_WARN")
 elseif (loglevel STREQUAL "error")
-    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_ERROR")
+    set(SPDLOG_ACTIVE_LEVEL "SPDLOG_LEVEL_ERROR")
 endif()
+get_target_property(defs spdlog COMPILE_DEFINITIONS
+   )
+get_target_property(idefs spdlog INTERFACE_COMPILE_DEFINITIONS
+   )
 set_target_properties(spdlog PROPERTIES
-    INTERFACE_COMPILE_DEFINITIONS
-        "${SPDLOG_ACTIVE_LEVEL}"
+   COMPILE_DEFINITIONS
+       "SPDLOG_ACTIVE_LEVEL=${SPDLOG_ACTIVE_LEVEL};${defs}"
+   INTERFACE_COMPILE_DEFINITIONS
+       "SPDLOG_ACTIVE_LEVEL=${SPDLOG_ACTIVE_LEVEL};${idefs}"
+       )
+add_library(spdlog_and_fmt INTERFACE)
+target_link_libraries(spdlog_and_fmt INTERFACE
+    spdlog::spdlog
+    fmt::fmt
     )
+add_library(cmake_utils::spdlog_and_fmt ALIAS spdlog_and_fmt)
