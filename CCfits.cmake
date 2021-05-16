1708af1813efcd8aca9692c12d0e97789f08d107
zhiyuanma <zhiyuanma@umass.edu>
2021-05-16 17:30:49 -0400

    proper refactor of ccfits

--- CCfits.cmake
+++ CCfits.cmake
@@ -1,102 +1,52 @@
 include_guard(GLOBAL)
-include(FetchContent)
-include(ExternalProject)
-find_program(MAKE_EXE NAMES gmake nmake make)
+include(ExternalProjectHelper)
 
-set(cfitsio_url "http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz")
-set(cfitsio_prefix "${CMAKE_BINARY_DIR}/_eps")
-set(cfitsio_src "${cfitsio_prefix}/src/cfitsio")
-set(cfitsio_configure ${cfitsio_src}/configure --enable-ssse3 --disable-curl --prefix=${cfitsio_prefix})
-set(cfitsio_make ${MAKE_EXE} libcfitsio.a)
-set(cfitsio_install ${MAKE_EXE} install)
-
-FetchContent_Declare(
+ExternalProjectHelper(
     cfitsio
-    URL ${cfitsio_url}
-    LOG_DOWNLOAD 1
+    CONTENT_DECLARE
+        URL "http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz"
+    PROJECT_ADD
+        BUILD_IN_SOURCE 1
+    CONFIGURE_ARGS
+        --enable-ssse3 --disable-curl
+    MAKE_ARGS
+        libcfitsio.a
+    MAKE_INSTALL_ARGS
+        ""
+    LIBS
+        libcfitsio.a
+    INCLUDE_SOURCE_DIRS
+        "."
     )
-FetchContent_GetProperties(cfitsio)
-if(NOT cfitsio_POPULATED)
-    message("Setting up cfitsio from ${cfitsio_url}")
-    FetchContent_Populate(cfitsio)
-    set(cfitsio_SOURCE_DIR ${cfitsio_SOURCE_DIR} PARENT_SCOPE)
-    set(cfitsio_BINARY_DIR ${cfitsio_BINARY_DIR} PARENT_SCOPE)
-endif()
-ExternalProject_Add(
-    cfitsio
-    PREFIX ${cfitsio_prefix}
-    URL ${cfitsio_url}
-    LOG_DOWNLOAD 1
-    LOG_BUILD 1
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ${cfitsio_configure}
-    BUILD_COMMAND ${cfitsio_make}
-    INSTALL_COMMAND ${cfitsio_install}
-    BUILD_BYPRODUCTS ${cfitsio_prefix}/lib/libcfitsio.a
-    )
-
-add_library(ep_cfitsio STATIC IMPORTED)
-set_property(TARGET ep_cfitsio PROPERTY IMPORTED_LOCATION ${cfitsio_prefix}/lib/libcfitsio.a)
-# this is need to make sure the include dir exists in config time.
-file(MAKE_DIRECTORY ${cfitsio_prefix}/include)
-set_property(TARGET ep_cfitsio PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${cfitsio_prefix}/include ${cfitsio_SOURCE_DIR})
-add_dependencies(ep_cfitsio cfitsio)
-add_library(cmake_utils::cfitsio ALIAS ep_cfitsio)
+add_library(cmake_utils::cfitsio ALIAS eph_cfitsio)
 
 # CCfits
-set(CCfits_url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz")
-set(CCfits_prefix "${CMAKE_BINARY_DIR}/_eps")
-set(CCfits_src "${CCfits_prefix}/src/CCfits")
-set(CCfits_configure ${CCfits_src}/configure --prefix=${CCfits_prefix} --with-cfitsio=${cfitsio_prefix} --enable-static --disable-shared)
-set(CCfits_make ${MAKE_EXE} libCCfits.la)
-set(CCfits_install ${MAKE_EXE} install)
-
-FetchContent_Declare(
-    CCfits
-    URL ${CCfits_url}
-    LOG_DOWNLOAD 1
+ExternalProjectHelper(
+    ccfits
+    CONTENT_DECLARE
+        URL "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
+    PROJECT_ADD
+        BUILD_IN_SOURCE 1
+    CONFIGURE_ARGS
+        --with-cfitsio=${EPH_PREFIX} --enable-static --disable-shared
+    MAKE_ARGS
+        libCCfits.la
+    MAKE_INSTALL_ARGS
+        ""
+    LIBS
+        libCCfits.a
+    INCLUDE_SOURCE_DIRS
+        "include"
+    DEPS
+        cfitsio
     )
-FetchContent_GetProperties(CCfits)
-if(NOT CCfits_POPULATED)
-    message("Setting up CCfits from ${CCfits_url}")
-    FetchContent_Populate(CCfits)
-    set(ccfits_SOURCE_DIR ${ccfits_SOURCE_DIR} PARENT_SCOPE)
-    set(ccfits_BINARY_DIR ${ccfits_BINARY_DIR} PARENT_SCOPE)
-     # copy ccfits headers to a directory
-    file(GLOB CCfits_headers
-       "${ccfits_SOURCE_DIR}/*.h"
-    )
-    set(CCfits_headers ${CCfits_headers} ${ccfits_SOURCE_DIR}/CCfits)
-    set(CCfits_tmp_include_dir ${ccfits_SOURCE_DIR}/include)
-    set(CCfits_tmp_headers_dir ${CCfits_tmp_include_dir}/CCfits)
-    file(MAKE_DIRECTORY ${CCfits_tmp_headers_dir})
-    file(COPY ${CCfits_headers} DESTINATION ${CCfits_tmp_headers_dir})
-endif()
-
-ExternalProject_Add(
-    CCfits
-    PREFIX ${CCfits_prefix}
-    URL ${CCfits_url}
-    LOG_DOWNLOAD 1
-    LOG_BUILD 1
-    BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ${CCfits_configure}
-    BUILD_COMMAND ${CCfits_make}
-    INSTALL_COMMAND ${CCfits_install}
-    BUILD_BYPRODUCTS ${CCfits_prefix}/lib/libCCfits.a
-    )
-
-# make sure ccfits compiles after cfitsio
-add_dependencies(CCfits cfitsio)
-
-add_library(ep_CCfits STATIC IMPORTED)
-set_property(TARGET ep_CCfits PROPERTY IMPORTED_LOCATION ${CCfits_prefix}/lib/libCCfits.a)
-# this is need to make sure the include dir exists in config time.
-file(MAKE_DIRECTORY ${CCfits_prefix}/include)
-set_property(TARGET ep_CCfits PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CCfits_prefix}/include ${CCfits_tmp_include_dir})
-target_link_libraries(ep_CCfits
-    INTERFACE
-    ep_cfitsio
-    )
-add_dependencies(ep_CCfits CCfits)
-add_library(cmake_utils::CCfits ALIAS ep_CCfits)
+message("Setting up CCfits headers")
+file(GLOB CCfits_headers "${ccfits_SOURCE_DIR}/*.h")
+set(CCfits_headers ${CCfits_headers} ${ccfits_SOURCE_DIR}/CCfits)
+set(CCfits_tmp_include_dir ${ccfits_SOURCE_DIR}/include)
+set(CCfits_tmp_headers_dir ${CCfits_tmp_include_dir}/CCfits)
+file(MAKE_DIRECTORY ${CCfits_tmp_headers_dir})
+file(COPY ${CCfits_headers} DESTINATION ${CCfits_tmp_headers_dir})
+add_library(cmake_utils::CCfits ALIAS eph_ccfits)
+# print_target_properties(eph_cfitsio)
+# print_target_properties(eph_ccfits)
---
+++ ExternalProjectHelper.cmake
@@ -0,0 +1,74 @@
+include_guard(GLOBAL)
+include(FetchContent)
+include(ExternalProject)
+
+set(EPH_PREFIX "${CMAKE_BINARY_DIR}/_eps")
+set(EPH_LIBDIR ${EPH_PREFIX}/lib)
+set(EPH_INCDIR ${EPH_PREFIX}/include)
+file(MAKE_DIRECTORY ${EPH_INCDIR})
+
+function(ExternalProjectHelper name)
+    set(mvargs CONTENT_DECLARE PROJECT_ADD CONFIGURE_ARGS MAKE_ARGS MAKE_INSTALL_ARGS LIBS INCLUDE_SOURCE_DIRS DEPS)
+    cmake_parse_arguments(PARSE_ARGV 1 EPH "" "" "${mvargs}")
+
+    set(declare_args
+        ${name}
+        ${EPH_CONTENT_DECLARE}
+        LOG_DOWNLOAD ON
+        )
+    message(STATUS "declare external project ${name}: ${declare_args}")
+    FetchContent_Declare(${declare_args})
+    FetchContent_GetProperties(${name})
+    if(NOT ${name}_POPULATED)
+        message("Setting up ${name} with ${EPH_CONTENT_DECLARE}")
+        FetchContent_Populate(${name})
+        set(${name}_SOURCE_DIR ${${name}_SOURCE_DIR} PARENT_SCOPE)
+        set(${name}_BINARY_DIR ${${name}_BINARY_DIR} PARENT_SCOPE)
+    endif()
+    set(build_byproducts "${EPH_LIBS}")
+    list(TRANSFORM build_byproducts PREPEND "${EPH_LIBDIR}/")
+    message(STATUS "build byproducts: ${build_byproducts}")
+    # file(MAKE_DIRECTORY ${name}_SOURCE_DIR)
+    # file(WRITE "${name}_SOURCE_DIR/eph_source_tag" "${name}")
+    find_program(MAKE_EXE NAMES gmake nmake make)
+    ExternalProject_Add(
+        ${name}
+        SOURCE_DIR ${${name}_SOURCE_DIR}
+        PREFIX ${EPH_PREFIX}
+        LOG_BUILD ON
+        ${EPH_PROJECT_ADD}
+        CONFIGURE_COMMAND ./configure --prefix=${EPH_PREFIX} ${EPH_CONFIGURE_ARGS}
+        BUILD_COMMAND ${MAKE_EXE} ${EPH_MAKE_ARGS}
+        INSTALL_COMMAND ${MAKE_EXE} install ${EPH_MAKE_INSTALL_ARGS}
+        BUILD_BYPRODUCTS ${build_byproducts}
+        )
+    set(inc_src_dirs ${EPH_INCLUDE_SOURCE_DIRS})
+    list(TRANSFORM inc_src_dirs PREPEND ${${name}_SOURCE_DIR}/)
+    message(STATUS "include source dirs: ${inc_src_dirs}")
+    set(static_libs)
+    foreach(lib ${EPH_LIBS})
+        get_filename_component(libstem ${lib} NAME_WE  [CACHE])
+        set(target_name eph_${name}_${libstem})
+        set(static_libs ${static_libs} ${target_name})
+        add_library(${target_name} STATIC IMPORTED GLOBAL)
+        set_property(TARGET ${target_name} PROPERTY IMPORTED_LOCATION ${EPH_LIBDIR}/${lib})
+        set_property(TARGET ${target_name} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${EPH_INCDIR} ${inc_src_dirs})
+    endforeach()
+    # set(EPH_${NAME}_LIBS ${static_libs} PARENT_SCOPE)
+    message(STATUS "static libs: ${static_libs}")
+
+    set(dep_libs)
+    foreach(dep ${EPH_DEPS})
+        add_dependencies(${name} eph_${dep})
+        set(dep_libs ${dep_libs} eph_${dep})
+    endforeach()
+    message(STATUS "dep libs: ${dep_libs}")
+
+    add_library(eph_${name} INTERFACE)
+    target_link_libraries(
+        eph_${name} INTERFACE
+        ${static_libs}
+        ${dep_libs}
+        )
+    add_dependencies(eph_${name} ${name})
+endfunction()
