544457faec8a146a88b12e0434cf176c2fa9ad10
Michael McCrackan <mmccrackan@umass.edu>
2021-08-10 23:32:10 -0400

    updated cfitsio paths

--- CCfits.cmake
+++ CCfits.cmake
@@ -28,18 +28,21 @@ ExternalProjectHelper(
     PROJECT_ADD
         BUILD_IN_SOURCE 1
     CONFIGURE_ARGS
-        --with-cfitsio=${EPH_PREFIX} --enable-static --disable-shared
+    --with-cfitsio-include=${cfitsio_SOURCE_DIR} --with-cfitsio-libdir=${EPH_PREFIX} --enable-static --disable-shared
     MAKE_ARGS
         libCCfits.la
     MAKE_INSTALL_ARGS
         install-data-am install-libLTLIBRARIES
     LIBS
         libCCfits.a
-    INCLUDE_SOURCE_DIRS
-        "include"
+
     DEPS
         cfitsio
     )
+
+
+message(${cfitsio_SOURCE_DIR})
+
 message("Setting up CCfits headers")
 file(GLOB CCfits_headers "${ccfits_SOURCE_DIR}/*.h")
 set(CCfits_headers ${CCfits_headers} ${ccfits_SOURCE_DIR}/CCfits)
@@ -47,6 +50,6 @@ set(CCfits_tmp_include_dir ${ccfits_SOURCE_DIR}/include)
 set(CCfits_tmp_headers_dir ${CCfits_tmp_include_dir}/CCfits)
 file(MAKE_DIRECTORY ${CCfits_tmp_headers_dir})
 file(COPY ${CCfits_headers} DESTINATION ${CCfits_tmp_headers_dir})
-add_library(cmake_utils::CCfits ALIAS eph_ccfits)
+add_library(cmake_utils::ccfits ALIAS eph_ccfits)
 # print_target_properties(eph_cfitsio)
 # print_target_properties(eph_ccfits)
